18d95cb3c7afa86ae2ea0c0ca5c91eb8ea59bdd2
JesusCoyotzi <jesus_coyotzi@hotmail.com>
2020-04-18 17:54:52 -0500

    Added full functionality

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # python-gearing
+A very simple python utility to draw involute, metric spur gears. 
+Simply run from the command line:
+```bash
+python gears.py -h
+usage: gears.py [-h] --module MODULE --n N [--store] [--noplot]
+
+Python involute gear generation
+
+optional arguments:
+  -h, --help       show this help message and exit
+  --module MODULE  Gear module, only gears with the same module can mesh
+  --n N            Number of teeth
+  --store          Store gear as plain text
+  --noplot         Plot Gear
+```
+The --store parameter allows to store the list of 2D points that conform the gear to a text file on disk. 
+The name of the file is gear-{module}-{teeth}.gear. 
+
+## Dependencies
+Python3 and Numpy. That is all!
+
+## TODO
+Perhaps adding support for DXF export
--- gears.py
+++ gears.py
@@ -1,11 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import argparse
+
 
 class Gear(object):
     """Simple Gear object in python."""
     addendum = 1.0
     dedendum = 1.25
-    def __init__(self, modulo,teeth,pressure_angle=20.0):
+
+    def __init__(self, modulo, teeth, pressure_angle=20.0):
         self.modulo = modulo
         self.teeth = teeth
         self.pressure_angle = 20.0
@@ -19,83 +22,131 @@ class Gear(object):
         self.out_radius = self.pitch_diameter/2 + self.addendum
         self.in_radius = self.pitch_diameter/2 - self.dedendum
 
-    def make_involute(self,points=20):
+    def make_involute(self, points=20):
         """Base involute"""
-        t = np.linspace(0,np.pi/6,num=points)
+        t_last = np.sqrt(np.square(self.out_radius) /
+                         np.square(self.base_radius) - 1)
+        t = np.linspace(0, t_last, num=points)
         inv_x = self.base_radius * (np.cos(t)+(t)*np.sin(t))
         inv_y = self.base_radius * (np.sin(t)-(t)*np.cos(t))
-        self.involute = np.stack((inv_x,inv_y),axis=1)
+        self.involute = np.stack((inv_x, inv_y), axis=1)
         return
 
     def make_tooth(self):
         """Create base all tooth"""
-        ang_pitch = 2* np.pi / (self.teeth * 2 * 2) #Radians per teeth or valley
-        low_tooth = self._rotate(ang_pitch,self.involute)
+        # There are 2pi radians in a circle, a gear with n teeth will need
+        # One teeth-valley every 2pi/teeth radians
+        # We offset the first tooth half that angular distance to center the involute with
+        # respect the x-axis
+        # Radians per teeth or valley
+        ang_pitch = 2 * np.pi / (self.teeth * 2 * 2)
+        # add dedendum
+        full_depth = np.insert(self.involute, 0, [self.in_radius, 0], axis=0)
+        low_tooth = self._rotate(ang_pitch, full_depth)
         high_tooth = np.copy(low_tooth)
-        high_tooth[:,1] = -1 * low_tooth[:,1]
-        self.base_tooth = np.concatenate((high_tooth,low_tooth))
+        high_tooth[:, 1] = -1 * low_tooth[:, 1]
+        high_tooth = high_tooth[::-1]
+        self.base_tooth = np.concatenate((low_tooth, high_tooth))
         return
 
     def generate_gear(self):
         """Use base tooth to get full gear profile"""
         self.make_involute()
         self.make_tooth()
+        start_angle = 2 * np.pi / (self.teeth)
         self.gear_profile = np.copy(self.base_tooth)
-        for t in np.linspace(0,2*np.pi,self.teeth):
-            next_tooth = self._rotate(t,self.base_tooth)
-            self.gear_profile = np.concatenate((next_tooth,self.gear_profile))
+        for t in np.linspace(start_angle, 2*np.pi, self.teeth, endpoint=False):
+            next_tooth = self._rotate(t, self.base_tooth)
+            self.gear_profile = np.concatenate((next_tooth, self.gear_profile))
 
         return
 
-    def _rotate(self,theta,v):
+    def _rotate(self, theta, v):
         """Rotate v arrary theta radians"""
-        c,s =np.cos(theta),np.sin(theta)
-        R = np.array( ( (c,-s) , (s,c) ) )
-        rot = np.dot(v,R)
+        c, s = np.cos(theta), np.sin(theta)
+        R = np.array(((c, -s), (s, c)))
+        rot = np.dot(v, R)
         return rot
 
 
-def involute(r,a,points=100):
-    t = np.linspace(0,np.pi/4,num=points)
-    x = r *(np.cos(t)+(t-a)*np.sin(t))
-    y = r *(np.sin(t)-(t-a)*np.cos(t))
-    return x,y
-    #return np.stack((x,y),axis=1)
-
-def rotation(theta,v):
-    c,s =np.cos(theta),np.sin(theta)
-    R = np.array( ( (c,-s) , (s,c) ) )
-    rot = np.dot(v,R)
-    return rot
-
-if __name__ == '__main__':
-    radius = 2
-
-    my_gear = Gear(10,25)
-    my_gear.generate_gear()
-    #print(my_gear.base_tooth)
-
-    inv_x, inv_y = involute(radius,0,20)
-    radial_step = 360/(20*2*2)
-    xy = np.stack((inv_x,inv_y),axis=1)
-    xy = rotation(np.radians(radial_step),xy)
-
-    xy_mirror = np.copy(xy)
-    xy_mirror[:,1] = xy[:,1] * -1
-
-    xy = np.concatenate((xy, xy_mirror),axis=0)
-
-    pitch_circle = plt.Circle((0, 0), my_gear.pitch_diameter /2 , color='r',fill=False)
-    root_circle = plt.Circle((0, 0), my_gear.in_radius , color='b',fill=False)
-    ext_circle = plt.Circle((0, 0), my_gear.out_radius , color='g',fill=False)
-    base_circle = plt.Circle((0, 0), my_gear.base_radius , color='y',fill=False)
+def draw_gear(gear):
+    pitch_circle = plt.Circle(
+        (0, 0), gear.pitch_diameter / 2, color='r', fill=False)
+    root_circle = plt.Circle((0, 0), gear.in_radius, color='b', fill=False)
+    ext_circle = plt.Circle((0, 0), gear.out_radius, color='g', fill=False)
+    base_circle = plt.Circle((0, 0), gear.base_radius, color='y', fill=False)
     fig, ax = plt.subplots()
-    #plt.xlim(*my_gear.pitch_diameter /2,2*my_gear.pitch_diameter /2)
+    # plt.xlim(*my_gear.pitch_diameter /2,2*my_gear.pitch_diameter /2)
     #plt.ylim(-2*my_gear.pitch_diameter /2,2*my_gear.pitch_diameter /2)
     ax.set_aspect(1)
     ax.add_artist(pitch_circle)
     ax.add_artist(root_circle)
     ax.add_artist(ext_circle)
     ax.add_artist(base_circle)
-    ax.plot(my_gear.gear_profile[:,0],my_gear.gear_profile[:,1])
+    ax.plot(gear.gear_profile[:, 0], gear.gear_profile[:, 1])
     plt.show()
+
+
+# Test functions
+def involute(r, a, points=100):
+    t = np.linspace(0, np.pi/4, num=points)
+    x = r * (np.cos(t)+(t-a)*np.sin(t))
+    y = r * (np.sin(t)-(t-a)*np.cos(t))
+    return x, y
+    # return np.stack((x,y),axis=1)
+
+
+def rotation(theta, v):
+    c, s = np.cos(theta), np.sin(theta)
+    R = np.array(((c, -s), (s, c)))
+    rot = np.dot(v, R)
+    return rot
+
+
+def make_profile(radius, teeth):
+    inv_x, inv_y = involute(radius, 0, 20)
+    radial_step = 360/(teeth*2*2)
+    xy = np.stack((inv_x, inv_y), axis=1)
+    xy = rotation(np.radians(radial_step), xy)
+
+    w_x, w_y = involute(radius, 0, 20)
+    wxy = np.stack((inv_x, -inv_y), axis=1)
+    xy = np.concatenate((xy, wxy), axis=0)
+    return xy
+
+
+def make_profile_rot(radius, teeth):
+    inv_x, inv_y = involute(radius, 0, 20)
+    radial_step = 360/(teeth*2*2)
+    xy = np.stack((inv_x, inv_y), axis=1)
+    xy = rotation(np.radians(radial_step), xy)
+
+    wxy = np.copy(xy)
+    wxy[:, 1] = -1 * wxy[:, 1]
+    xy = np.concatenate((xy, wxy), axis=0)
+    return xy
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Python involute gear generation')
+    parser.add_argument('--module', action='store', type=float,
+                        help='Gear module, only gears with the same module can mesh',
+                        required=True)
+    parser.add_argument('--n', action='store', type=int,
+                        help='Number of teeth',
+                        required=True)
+    parser.add_argument('--store', action='store_true', default=False,
+                        help='Store gear as plain text')
+    parser.add_argument('--noplot', action='store_true', default=False,
+                        help='Plot Gear')
+    args = parser.parse_args()
+
+    inv_gear = Gear(args.module, args.n)
+    inv_gear.generate_gear()
+
+    if not args.noplot:
+        draw_gear(inv_gear)
+    elif args.store:
+        np.savetxt("gear-{}-{}.gear".format(args.module, args.n),
+                   inv_gear.gear_profile)
